edfeb82185e71c29bc3d81cfda91a7a25c93076d
Aditya Sharma <102982655+ez4Adit@users.noreply.github.com>
2022-10-20 16:51:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello ez4Adit!\n'
+  const msg = 'Hello ez4Adit Present here!\n'
   res.end(msg);
 });
 
